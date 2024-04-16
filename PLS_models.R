4425f73de5b86fd795c2babea8074c3ebeac8d7b
forestiy <p.mallior@gmail.com>
2024-04-17 00:31:14 +0200

    Update PLS_models.R

--- PLS_models.R
+++ PLS_models.R
@@ -1,5 +1,7 @@
 #This file contains the code for both outcomes AMU and respiratory, changing the model from plsRlm to plsR and 
 #the outcome from Respiratory to AMU is needed to run selection for AMU; now it is set for counts of respiratory treatments 
+
+#Power analysis is at the bottom oft
 #::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
 #________________Clear_code_for_dependent_PLS____________________
 rm(list=ls()) 
@@ -372,3 +374,206 @@ for (o in c(1,rep(c(2,3,4,6,9),20))){
 
 PREDS_list<-c(PREDS_list,names(which.max(table(top_model))),table(top_model))
 
+
+
+
+
+
+
+
+#################################################################################################
+#Power analysis using pwr.f2test()
+#################################################################################################
+#setting up bootstrap to identify significance of all the variables involved 
+n_rep<-1
+store_df<-NULL
+while(n_rep<2001){
+  
+  ndim<-21
+  # with this functino we generate multivariate random variables usign the covariance structure of the observed data
+  #of the selected AMU model
+  DF_boot<- mvrnorm(n=sum(185 , 64),mu=colMeans(cbind(Outcomes,Predictors)),Sigma=cov(cbind(Outcomes,Predictors))) )
+
+  #This the bootstrap
+for ( N_req in c(c(20,30),seq(40,270,10),276) ) {
+
+  DF_boot_n<-DF_boot[sample(c(1:nrow(DF_boot)),N_req,replace=T),]
+ 
+  
+  
+  
+Outcomes_boot<-as.matrix(cbind(DF_boot_n[,1]))
+Predictors_boot<-as.matrix(cbind(DF_boot_n[,-1]))
+
+#CV 10 fold to reduce overfitting and compare best case with 36
+#cross_validation
+ids<-sample(1:nrow(DF_boot_n),nrow(DF_boot_n),replace=F)
+test_ids<-split(ids, ceiling(seq_along(ids)/ifelse(N_req<=21,3,
+                                                   round(length(ids)/10,0))))
+
+test_id<-list()
+train_id<-list()
+for(w in 1:length(test_ids)){
+  test_id[[w]]<-as.numeric(unlist(test_ids[w]))
+  train_id[[w]]<-sort(ids)[-as.numeric(unlist(test_ids[w]))]
+}
+
+
+
+
+stor_rmse<-c()
+stor_RSR<-c()
+stor_coeflm1<-c()
+stor_coeflm2<-c()
+stor_PVlm1<-c()
+stor_PVlm2<-c()
+stor_efstdS1<-c()
+stor_efstdS2<-c()
+stor_power_test<-c()
+stor_power_train<-c()
+stor_R2_test<-c()
+stor_R2_train<-c()
+stor_f2_train<-c()
+stor_f2_test<-c()
+stor_RS1_pvalue<-c()
+for(ui in 1:length(test_id)){
+  if(length(unlist(test_id[ui]))>=3){
+modpls <-NULL
+
+    modpls <- plsR(Outcomes_boot[unlist(train_id[ui]) ],verbose=F,
+               Predictors_boot[unlist(train_id[ui]),],scaleX = T, scaleY = T,nt=2)
+
+# Sample real and predicted values
+lm_model<-lm(Outcomes_boot[unlist(train_id[ui]) ] ~ modpls$tt[,1] + modpls$tt[,2])
+
+
+train_LMcoef_1<-coefficients(lm_model)[2]
+train_LMcoef_2<-coefficients(lm_model)[3]
+train_LMpval_1<-coef(summary(lm_model))[2,4]
+train_LMpval_2<-coef(summary(lm_model))[3,4]
+
+effect_sizeD1<-train_LMcoef_1/sd(Outcomes_boot[unlist(train_id[ui]) ])
+effect_sizeD2<-train_LMcoef_2/sd(Outcomes_boot[unlist(train_id[ui]) ])
+
+
+#test
+real_values <- DF_boot_n[unlist(test_id[ui]),1]
+
+#train
+real_values_train <- DF_boot_n[unlist(train_id[ui]),1]
+
+if(length(unlist(test_id[ui]))==1){
+  
+  #test for R2 out of sample
+  newwd<-data.frame(DF_boot_n[unlist(test_id[ui]),-1])
+  colnames(newwd)[1]<-""
+  predicted_values <- as.vector(predict.plsRmodel(modpls,newdata= t(newwd) ))
+  
+  
+  #train fit for in sample R2
+  neewdtrain<-data.frame(DF_boot_n[unlist(train_id[ui]),-1])
+  colnames(neewdtrain)[1]<-""
+  predicted_values_train <- as.vector(predict.plsRmodel(modpls,newdata= t(neewdtrain) ))
+  
+}else{
+  #TEST
+predicted_values <- as.vector(predict.plsRmodel(modpls,newdata=DF_boot_n[unlist(test_id[ui]),-1]))
+
+
+#train
+predicted_values_train <- as.vector(predict.plsRmodel(modpls,newdata=DF_boot_n[unlist(train_id[ui]),-1]))
+
+}
+
+
+Rsquared_test<-1 - (mean((real_values-predicted_values)^2) / mean((real_values - mean(real_values_train))^2))
+
+
+Rsquared_train<-1 - (sum((real_values_train-predicted_values_train)^2) / sum((real_values_train - mean(real_values_train))^2))
+
+power_train<-pwr.f2.test(u = 22, 
+                         v = round.off(length(unlist(train_id[ui])) -  as.numeric(modpls$ic.dof[,1][3])),
+                         f2 = (Rsquared_train/(1 - Rsquared_train)), #0.27
+                         sig.level = 0.05)$power
+if(Rsquared_test==-Inf | Rsquared_test<0 ){
+  power_test<-0
+  
+ 
+}else{
+tryCatch({
+
+ # modpls$ic.dof[,1][3]
+  
+power_test<-pwr.f2.test(u = 22, 
+                       v = round.off(length(unlist(train_id[ui])) -  as.numeric(modpls$ic.dof[,1][3])),
+                       f2 =(Rsquared_test/(1 - Rsquared_test)), #0.27
+                       sig.level = 0.05)$power
+
+
+
+
+},error=function(e){power_test<-NA;break;print("OVERCOMING ERROR!!!!!!!!!")})
+}
+
+S1_pvalue<-ifelse(Rsquared_test>=R_S1,1,0)
+
+R2_test<-Rsquared_test
+R2_train<-Rsquared_train
+
+f2_test<-Rsquared_test/(1 - Rsquared_test)
+f2_train<-Rsquared_train/(1 - Rsquared_train)
+
+
+stor_rmse<-append(stor_rmse,rmse(real_values,predicted_values))
+stor_RSR<-append(stor_RSR,rmse(real_values,predicted_values)/sd(real_values))
+stor_coeflm1<-append(stor_coeflm1,train_LMcoef_1)
+stor_coeflm2<-append(stor_coeflm2,train_LMcoef_2)
+stor_PVlm1<-append(stor_PVlm1,train_LMpval_1)
+stor_PVlm2<-append(stor_PVlm2,train_LMpval_2)
+stor_efstdS1<-append(stor_efstdS1,effect_sizeD1)
+stor_efstdS2<-append(stor_efstdS2,effect_sizeD2)
+stor_power_test<-append(stor_power_test,power_test)
+stor_power_train<-append(stor_power_train,power_train)
+stor_R2_test<-append(stor_R2_test,R2_test)
+stor_R2_train<-append(stor_R2_train,R2_train)
+stor_f2_test<-append(stor_f2_test,f2_test)
+stor_f2_train<-append(stor_f2_train,f2_train)
+
+stor_RS1_pvalue<-append(stor_RS1_pvalue,S1_pvalue)
+  }
+}
+
+
+if(is.null(modpls)){
+  znfg<-cbind(n_rep,N_req,rep(NA,12))
+}else{
+znfg<-cbind(n_rep,N_req,
+        RMSE=mean(stor_rmse,na.rm=T),
+     RSR=mean(stor_RSR,na.rm=T),
+     lmC_1= mean(stor_coeflm1),
+       lmC2=mean(stor_coeflm2),
+pval1=mean(stor_PVlm1,na.rm=T),
+pval2=mean(stor_PVlm2,na.rm=T),
+stdC1=mean(stor_efstdS1,na.rm=T),
+stdC2=mean(stor_efstdS2,na.rm=T),
+PowerTest=mean(stor_power_test,na.rm=T),
+PowerTrain=mean(stor_power_train,na.rm=T),
+R2_test=mean(stor_R2_test,na.rm=T),
+R2_train=mean(stor_R2_train,na.rm=T),
+f2_test=mean(stor_f2_test,na.rm=T),
+f2_train=mean(stor_f2_train,na.rm=T),
+RS1_pvalue=mean(stor_RS1_pvalue,na.rm=T))
+
+           #AIC= modpls$AIC[3],Rsq=modpls$R2[2])
+
+}
+
+store_df<-rbind(store_df,znfg)
+
+
+
+}
+  print(n_rep)
+n_rep<-n_rep+1
+
+}
